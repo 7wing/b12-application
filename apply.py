04bd9fdd0ee64171ffb20ee95bd327740b0a40f9
7wing <marthakamau0000@gmail.com>
2026-02-24 13:50:46 +0300

    Apply.py Updated

---
+++ apply.py
@@ -0,0 +1,22 @@
+import json, hmac, hashlib, requests
+from datetime import datetime, timezone
+
+payload = {
+    "email": "marthakamau0001@gmail.com",
+    "name": "Martha Kamau",
+    "repository_link": "https://github.com/7wing/b12-application",
+    "resume_link": "https://docs.google.com/document/d/17cHj30eMh56UWQxoSVGHKVnUomznvM_8TaWuU7wZdnk/edit?usp=sharing",
+    "timestamp": datetime.now(timezone.utc).isoformat()
+}
+
+body = json.dumps(payload, separators=(',', ':'), sort_keys=True)
+signature = hmac.new(
+    key=b"hello-there-from-b12",
+    msg=body.encode("utf-8"),
+    digestmod=hashlib.sha256
+).hexdigest()
+
+headers = {"X-Signature-256": f"sha256={signature}"}
+response = requests.post("https://b12.io/apply/submission", data=body, headers=headers)
+
+print(response.text)
